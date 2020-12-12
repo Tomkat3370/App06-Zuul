cb543f9a08833080ad196b3886728c3011cf13af
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2020-12-12 00:31:37 +0000

    new rooms added

--- src/Map.java
+++ src/Map.java
@@ -3,7 +3,7 @@ public class Map
     private Room startRoom;
 
     /**
-     *
+     * Constructor for game map
      */
     public Map()
     {
@@ -15,14 +15,18 @@ public class Map
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room intracranialSpace, frontalLobe, concentrationCentre, writingCenter, olfactoryCenter parietalLobe, temporalLobe, occipitalLobe, cerebellum;
 
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        intracranialSpace = new Room("outside the brain");
+        frontalLobe = new Room("in the control panel of personality and communication");
+        concentrationCentre = new Room("in the concentration, planning and problem solver");
+        writingCenter = new Room("in the writing room");
+        olfactoryCenter = new Room("in the smell recognition room");
+        parietalLobe = new Room("in the physical sensory processor");
+        temporalLobe = new Room("in the auditory and emotion interpreter/memory creator");
+        occipitalLobe = new Room("in the visual interpreter");
+        cerebellum = new Room("in the voluntary movement coordinator");
 
         // initialise room exits
         outside.setExit("east", theater);
