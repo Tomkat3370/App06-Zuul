721675d61d1ba52cb304d7403f5474bf02f41275
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2021-01-18 22:34:54 +0000

    Items created

--- src/Items.java
+++ src/Items.java
@@ -1,4 +1,13 @@
 public enum Items
 {
     //TODO: Create Key fragments and items.... class might work better??
+    /**
+     * TOKENs are collected in each secondary room and are traded for KEYss to the next primary room.
+     *   Each TOKEN and KEY collected will increase the player experience. Food can be collected
+     *   in each room to increase the player energy.
+     */
+
+    CONCENTRATION_TOKEN, WRITING_TOKEN, OLFACTORY_TOKEN, HEARING_TOKEN, MEMORY_TOKEN, MUSIC_TOKEN, TOUCH_TOKEN,
+    TASTE_TOKEN, PUZZLE_TOKEN, VISION_TOKEN, READING_TOKEN, FRONTAL_KEY, TEMPORAL_KEY, PARIETAL_KEY,
+    OCCIPITAL_KEY, CEREBELLUM_KEY, BANANA, CHEESE, CRACKERS, SPINACH,
 }
