4a415b6d2ede9dd73b1770b07ed68650b8e3e9f6
Tomkat3370 <72070856+Tomkat3370@users.noreply.github.com>
2021-01-30 00:42:50 +0000

    updated

--- src/Items.java
+++ src/Items.java
@@ -1,42 +1,18 @@
-public enum Items
+import java.util.ArrayList;
+import java.util.EnumSet;
+enum Items
 {
-    //TODO: Create Key fragments and items.... class might work better??
-    /**
-     * TOKENs are collected in each secondary room and are traded for KEYs to the next primary room.
-     *   Each TOKEN and KEY collected will increase the player experience. Food can be collected
-     *   in each room to increase the player energy.
-     */
+    CONCENTRATION_TOKEN, WRITING_TOKEN, OLFACTORY_TOKEN,
+    HEARING_TOKEN, MEMORY_TOKEN, MUSIC_TOKEN, TOUCH_TOKEN,
+    TASTE_TOKEN, PUZZLE_TOKEN, VISION_TOKEN, READING_TOKEN,
 
-        CONCENTRATION_TOKEN("token"), WRITING_TOKEN("token"), OLFACTORY_TOKEN("token"),
-        HEARING_TOKEN("token"), MEMORY_TOKEN("token"), MUSIC_TOKEN("token"),
-        TOUCH_TOKEN("touch token"), TASTE_TOKEN("token"), PUZZLE_TOKEN("token"),
-        VISION_TOKEN("token"), READING_TOKEN("reading token"),
+    FRONTAL_KEY, TEMPORAL_KEY, PARIETAL_KEY,
+    OCCIPITAL_KEY, CEREBELLUM_KEY,
 
-        FRONTAL_KEY("key"), TEMPORAL_KEY("key"), PARIETAL_KEY("key"),
-        OCCIPITAL_KEY("key"), CEREBELLUM_KEY("key"),
+    BANANA, CHEESE, CRACKERS, SPINACH,
 
-        BANANA("food"), CHEESE("food"), CRACKERS("food"), SPINACH("food");
+    TROPHY;
 
-
-    // The item string.
-    private String itemString;
-
-    /**
-     * Initialise with the corresponding command string.
-     * @param itemString The command string.
-     */
-    Items(String itemString)
-    {
-        this.itemString = itemString;
-    }
-
-    /**
-     * @return The command word as a string.
-     */
-    public String toString()
-    {
-        return itemString;
-    }
 
     //The item quantity
     private int itemQuantity;
@@ -51,7 +27,7 @@ public enum Items
         return itemQuantity;
     }
 
-    public void increaseItemQuantity(int i)
+    public void increaseItemQuantity()
     {
         itemQuantity ++;
     }
